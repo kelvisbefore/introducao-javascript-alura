e04c856ce2c3bd11bcfa1d6a55e0707c6bd61c7b
Kelvis Borges <kelvis.borges@before.com.br>
2018-11-01 13:26:40 -0400

    Create arrays, loops, styles

--- js/principal.js
+++ js/principal.js
@@ -7,11 +7,12 @@ console.log(paciente)
 for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
+    console.log(paciente)
 
     var tdPeso = paciente.querySelector(".info-peso")
     var peso = tdPeso.textContent;
 
-    var tdAltura = document.querySelector(".info-altura")
+    var tdAltura = paciente.querySelector(".info-altura")
     var altura = tdAltura.textContent;
 
     var tdImc = paciente.querySelector(".info-imc")
@@ -19,24 +20,23 @@ for (var i = 0; i < pacientes.length; i++) {
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-
-
-    if (peso <= 0 || peso >= 1000) {
+    if (peso <= 0 || peso >= 100) {
         console.log("peso invalido")
         pesoEhValido = false;
-        tdImc.textContent = "Peso Inválido";
+        tdImc.textContent = "Peso Inválido";   
+        paciente.classList.add("paciente-invalido")
     }
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("altura inválida")
         alturaEhValida = false;
-        tdImc.textContent = "Altura invalida";
+        tdImc.textContent = "Altura invalida";        
     }
 
     if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
 
-        tdImc.textContent = imc;
+        tdImc.textContent = imc.toFixed(2);
     }
 
 
