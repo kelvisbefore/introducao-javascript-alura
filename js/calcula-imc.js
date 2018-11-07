e335472dec5dd6d5d39ba751df44669a9e618a2e
Kelvis Borges <kelvis.borges@before.com.br>
2018-11-07 13:32:40 -0300

    separate functions reponsability

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -17,25 +17,40 @@ for (var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc")
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 100) {
+    if (!pesoEhValido) {
         console.log("peso invalido")
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido";   
         paciente.classList.add("paciente-invalido")
     }
 
-    if (altura <= 0 || altura >= 3.00) {
+    if (!alturaEhValida) {
         console.log("altura inválida")
         alturaEhValida = false;
         tdImc.textContent = "Altura invalida";        
     }
 
     if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura);
+        var imc = calculaImc(peso, altura);
 
-        tdImc.textContent = imc.toFixed(2);
+        tdImc.textContent = imc;
     }
+}
+
+function validaPeso(peso) {
+    return (peso >= 0 && peso < 1000) ? true : false;
+}
+
+function validaAltura(altura) {
+    return (altura >= 0 && altura < 3.00) ? true : false;
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura)
+
+    return imc.toFixed(2);
 }
